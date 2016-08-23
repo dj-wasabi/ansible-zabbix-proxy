5a449db239e8e9c52732e59cbf529a0b0fec947a
Werner Dijkerman <werner@dj-wasabi.nl>
2016-08-23 14:26:48 +0200

    Added molecule tests

---
+++ tests/test_default.py
@@ -0,0 +1,50 @@
+import pytest
+import requests
+
+
+def test_zabbixproxy_running_and_enabled(Service):
+    zabbix = Service("zabbix-proxy")
+    assert zabbix.is_enabled
+    assert zabbix.is_running
+
+
+@pytest.mark.parametrize("proxy", [
+    ("zabbix-proxy-pgsql"),
+    ("zabbix-proxy-mysql"),
+])
+def test_zabbix_package(Package, TestinfraBackend, proxy,SystemInfo):
+    host = TestinfraBackend.get_hostname()
+    host = host.replace("-centos", "")
+    host = host.replace("-debian", "")
+
+    if host == proxy:
+        zabbix_proxy = Package(proxy)
+        assert zabbix_proxy.is_installed
+
+        if SystemInfo.distribution == 'debian':
+            assert zabbix_proxy.version.startswith("1:3.0")
+        elif SystemInfo.distribution == 'centos':
+            assert zabbix_proxy.version.startswith("3.0")
+
+
+def test_socket(Socket):
+    assert Socket("tcp://0.0.0.0:10051").is_listening
+
+
+def test_zabbix_proxy_dot_conf(File):
+    zabbix_proxy_conf = File("/etc/zabbix/zabbix_proxy.conf")
+    assert zabbix_proxy_conf.user == "zabbix"
+    assert zabbix_proxy_conf.group == "zabbix"
+    assert zabbix_proxy_conf.mode == 0o644
+
+    assert zabbix_proxy_conf.contains("ListenPort=10051")
+    assert zabbix_proxy_conf.contains("DBHost=localhost")
+    assert zabbix_proxy_conf.contains("DebugLevel=3")
+
+
+def test_zabbix_include_dir(File):
+    zabbix_include_dir = File("/etc/zabbix/zabbix_proxy.conf.d")
+    assert zabbix_include_dir.is_directory
+    assert zabbix_include_dir.user == "zabbix"
+    assert zabbix_include_dir.group == "zabbix"
+    # assert zabbix_include_dir.mode == 0o644
