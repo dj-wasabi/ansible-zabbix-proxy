c7512bc03e1e37e4a624d42d4ae1e8fb6b41452e
Werner Dijkerman <werner@dj-wasabi.nl>
2019-04-14 12:13:05 +0200

    Updated to Zabbix 4.2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -28,9 +28,9 @@ def test_zabbix_package(Package, TestinfraBackend, proxy, SystemInfo):
         assert zabbix_proxy.is_installed
 
         if SystemInfo.distribution in ['debian', 'ubuntu']:
-            assert zabbix_proxy.version.startswith("1:4.0")
+            assert zabbix_proxy.version.startswith("1:4.2")
         elif SystemInfo.distribution == 'centos':
-            assert zabbix_proxy.version.startswith("4.0")
+            assert zabbix_proxy.version.startswith("4.2")
 
 
 def test_socket(Socket):
